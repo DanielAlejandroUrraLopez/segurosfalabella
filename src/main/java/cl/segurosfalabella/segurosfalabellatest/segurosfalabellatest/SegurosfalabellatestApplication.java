aca67fd54b5c0e08414652d97cfaaa83e7e0339b
Daniel Urra <daniel.desarrolloweb@outlook.com>
2021-04-28 22:25:35 -0400

    proyecto base
    se inicia el proyecto

---
+++ src/main/java/cl/segurosfalabella/segurosfalabellatest/segurosfalabellatest/SegurosfalabellatestApplication.java
@@ -0,0 +1,13 @@
+package cl.segurosfalabella.segurosfalabellatest.segurosfalabellatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegurosfalabellatestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegurosfalabellatestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/segurosfalabella/segurosfalabellatest/segurosfalabellatest/SegurosfalabellatestApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.segurosfalabella.segurosfalabellatest.segurosfalabellatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegurosfalabellatestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
